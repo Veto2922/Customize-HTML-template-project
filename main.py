ebc45590f21aa9d4cbda50947c2f0d010a36de31
veto2922 <veto2922@gmail.com>
2025-04-09 19:00:58 +0200

    a.m let gemini select the images

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from src.llm_blocks_helper.chain_builder import ChainBuilder
 if __name__ == "__main__":
     # Example usage
     business_name = "Electro Pi"
-    business_description = "development of cutting-edge technology and AI solution and software."
+    business_description = "development of cutting-edge technology and AI solution and software. if you put any person images please put  persons wearing glasses only"
     
     api_key_loader = APIKeyLoader()
     model_loader = ModelLoader()
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     # Create an instance of HtmlProcessManager
     html_manager = HtmlProcessManager()
     json_placeholder = html_manager.get_json_placeholder(r'Placeholder_template\software_placeholders\placeholder.json')
+    image_placeholder = html_manager.get_json_placeholder(r'Placeholder_template\software_placeholders\image_placeholder.json')
     html_placeholder = html_manager.get_html_placeholder(r'Placeholder_template\software_placeholders\placeholder.html')
     
     customize_json_placeholder_llm =  CustomizeJsonPlaceholder(
@@ -28,18 +29,20 @@ if __name__ == "__main__":
                             chain_builder=chain_builder
                         )
     updated_json = customize_json_placeholder_llm.run(business_name, business_description, json_placeholder)
-    new_html = html_manager.generate_new_html(updated_json, html_placeholder)
+    
     print('this is json from model  ' , updated_json)
     
+    new_html = html_manager.generate_new_html(updated_json, html_placeholder)
+        
     # Example usage
     src_root_folder = r"Placeholder_template\software_images"
     dest_folder = r"technology-software_template\assets\img"
-    select_random_image_and_move_it_to_Html_src(src_root_folder, dest_folder)
+    
+    # select_random_image_and_move_it_to_Html_src(src_root_folder, dest_folder , image_placeholder , new_html)
     
     # Generate new HTML using the updated JSON object
     html_manager.save_new_html(new_html , r'technology-software_template\new_html.html')
 
-    # Print the updated JSON object
     
 
 
